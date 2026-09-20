147a88d5ee1634191f31ec4f17e3b77f9e708bec
NAPO-cyber <gopalverma2503@gmail.com>
2026-09-20 11:06:35 +0530

    added response DTO

---
+++ src/main/java/com/notes/app/dto/NoteResponse.java
@@ -0,0 +1,41 @@
+package com.notes.app.dto;
+
+import java.time.LocalDateTime;
+
+public class NoteResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+    public NoteResponse(Long id, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
