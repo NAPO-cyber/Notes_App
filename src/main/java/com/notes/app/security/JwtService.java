e9f232ddab839e6039b731daa7262b979930a52f
NAPO-cyber <gopalverma2503@gmail.com>
2026-09-20 14:04:58 +0530

    implemented jwt

---
+++ src/main/java/com/notes/app/security/JwtService.java
@@ -0,0 +1,28 @@
+package com.notes.app.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final String SECRET = "my-super-secret-key-that-is-at-least-32-characters-long";
+
+    private final SecretKey key =
+            Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(key)
+                .compact();
+    }
+}
