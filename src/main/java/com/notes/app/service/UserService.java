5526496095c5dd7d0a6c47bd821eb56d1ae2e408
NAPO-cyber <gopalverma2503@gmail.com>
2026-09-20 14:12:16 +0530

    injected jwtservice dep. & added login logic

--- src/main/java/com/notes/app/service/UserService.java
+++ src/main/java/com/notes/app/service/UserService.java
@@ -1,8 +1,10 @@
 package com.notes.app.service;
 
+import com.notes.app.dto.LoginRequest;
 import com.notes.app.dto.UserRequest;
 import com.notes.app.model.User;
 import com.notes.app.repository.UserRepository;
+import com.notes.app.security.JwtService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,12 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, JwtService jwtService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
     }
 
     public void register(UserRequest request) {
@@ -25,10 +29,20 @@ public class UserService {
         User user = new User();
         user.setUsername(request.getUsername());
 
-        user.setPassword(
-                passwordEncoder.encode(request.getPassword())
-        );
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
 
         userRepository.save(user);
+    }
+
+    public String login(LoginRequest request) {
+
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("Invalid username or password"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid username or password");
+        }
+
+        return jwtService.generateToken(user.getUsername());
     }
 }
